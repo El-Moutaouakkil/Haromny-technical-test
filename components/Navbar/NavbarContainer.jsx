2b70ab0ef2e19a3241affe319194f921c8f5ac68
mohammed <el.moutaouakkil@hotmail.com>
2023-07-31 16:19:09 +0100

    add box shadow to NavbarContainer component

--- components/Navbar/NavbarContainer.jsx
+++ components/Navbar/NavbarContainer.jsx
@@ -5,9 +5,8 @@ const navItems = [`Products`, "Templates", "Pricing", "Blog", "Sign in"];
 
 function NavbarContainer() {
     return (
-        <div className='flex mt-5'>
+        <div className='flex items-center  py-4 shadow-sm shadow-orange-200'>
             <h1 className='text-lg font-bold w-2/5  ml-40'>Slite</h1>{" "}
-            {/* logo */}
             <Navbar className='w-3/5 space-x-2' items={navItems} />
         </div>
     );
