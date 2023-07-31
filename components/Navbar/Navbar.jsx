4d100899b4b4f62a25861e26a829eff5963b7ba9
mohammed <el.moutaouakkil@hotmail.com>
2023-07-31 16:23:25 +0100

    move Hero component to components/home
    /home will have all of the component needed to be displayed in the home page

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar({ items }) {
             className={`flex items-center gap-3 text-sm font-semibold text-red-900`}>
             <ul className='flex gap-5'>
                 {items.map((item, index) => (
-                    <li key={index}>{item}</li>
+                    <li key={index} className="hover:cursor-pointer" >{item}</li>
                 ))}
             </ul>
             <ActionButtons />
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import NavbarContainer from "../components/Navbar/NavbarContainer";
-import Hero from "../components/hero/Hero";
+import Hero from "../components/home/hero/Hero";
 
 export default function Home() {
     return (
