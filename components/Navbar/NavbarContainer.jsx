185005c0df338ec1d4cdab7435137efb9d8c7b90
mohammed <el.moutaouakkil@hotmail.com>
2023-07-31 13:26:06 +0100

    implement NavbarContainer component component

---
+++ components/Navbar/NavbarContainer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const navItems = ["Products", "Templates", "Pricing", "Blog", "Sign in"];
+
+function NavbarContainer() {
+    return (
+        <div className='flex mt-5'>
+            <h1 className='text-lg font-bold w-2/5  ml-40'>Slite</h1>{" "}
+            {/* logo */}
+            <Navbar className='w-3/5 space-x-2' items={navItems} />
+        </div>
+    );
+}
+
+export default NavbarContainer;
